b56ed1af7ec4366702a1d537fd63b4ca03eaebb6
yasincet <93564146+yasincet@users.noreply.github.com>
2023-10-26 02:23:36 +0300

    public created

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    const blog = {
-        id: 1,
-        title: 'My First Blog',
-        description: 'This is my first blog'
-    };
-
-  res.send(blog);
+  res.render('index');
 });
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+
 
 const port = 3000;
 
